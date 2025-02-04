7b2199ba3919b9488a402fb64076e66e0909769a
Luizmiguel1784 <santosalmeidaluizmiguel28@gmail.com>
2025-02-04 10:05:13 -0300

    Add files via upload

---
+++ Calculadora_com_Switch.java
@@ -0,0 +1,47 @@
+package aula28janeiro;
+	import java.util.Scanner;
+public class Calculadora_com_Switch {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double x, y, resultado;
+		char operaçao;
+		
+
+		System.out.print("digite o primeiro numero ");
+		x = sc.nextDouble();
+		System.out.print("digite o segundo numero ");
+		y = sc.nextDouble();
+		
+		System.out.print("digite a operação (+, -, *, /): ");
+		operaçao = sc.next().charAt(0);
+		switch (operaçao) {
+		case '+': 
+			resultado = x + y;
+			System.out.println("Resultado: "+ resultado);
+			break;
+		case '-': 
+			resultado = x - y;
+			System.out.println("Resultado: "+ resultado);
+			break;
+		case '*': 
+			resultado = x * y;
+			System.out.println("Resultado: "+ resultado);
+			break;
+		case '/': 
+			if (y != 0) { 
+				resultado = x / y;
+				System.out.println("resultado: " + resultado);
+			} else {System.out.println("Erro Divisão por zero não permitida");
+			
+		
+		} break;
+		default:System.out.println("operação invalida");
+			
+		}sc.close();
+		
+	
+	}
+
+}
